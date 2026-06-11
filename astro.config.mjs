da99791cd18ac5f0227ce05fb0311ac32b93cb4c
Lennard <198783158+Le24367@users.noreply.github.com>
2026-06-11 21:19:14 +0200

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://beachandfood.de',
+  integrations: [
+    sitemap(),
+  ],
+})
